28a6acbfe149e66276dbea5af407d53799323690
Sharjeel-22 <pcraboot55441@gmail.com>
2023-12-27 03:00:56 +0500

    added new items

--- src/app/dashboard/grid-table/grid-table.component.ts
+++ src/app/dashboard/grid-table/grid-table.component.ts
@@ -11,8 +11,23 @@ export class GridTableComponent {
    rowData = [
     { mission: "Voyager", company: "NASA", location: "Cape Canaveral", date: "1977-09-05", rocket: "Titan-Centaur ", price: 86580000, successful: true,range:65000,spent_time:"50 hrs" },
     { mission: "Apollo 13", company: "NASA", location: "Kennedy Space Center", date: "1970-04-11", rocket: "Saturn V", price: 3750000, successful: false,range:64000,spent_time:"40 hrs" },
+    { mission: "Falcon 9", company: "SpaceX", location: "Cape Canaveral", date: "2015-12-22", rocket: "Falcon 9", price: 9750000, successful: true,range:75000,spent_time:"0 hrs"},
+    { mission: "Voyager", company: "NASA", location: "Cape Canaveral", date: "1977-09-05", rocket: "Titan-Centaur ", price: 86580000, successful: true,range:65000,spent_time:"50 hrs" },
+    { mission: "Apollo 13", company: "NASA", location: "Kennedy Space Center", date: "1970-04-11", rocket: "Saturn V", price: 3750000, successful: false,range:64000,spent_time:"40 hrs" },
+    { mission: "Falcon 9", company: "SpaceX", location: "Cape Canaveral", date: "2015-12-22", rocket: "Falcon 9", price: 9750000, successful: true,range:75000,spent_time:"0 hrs"},
+    { mission: "Voyager", company: "NASA", location: "Cape Canaveral", date: "1977-09-05", rocket: "Titan-Centaur ", price: 86580000, successful: true,range:65000,spent_time:"50 hrs" },
+    { mission: "Apollo 13", company: "NASA", location: "Kennedy Space Center", date: "1970-04-11", rocket: "Saturn V", price: 3750000, successful: false,range:64000,spent_time:"40 hrs" },
+    { mission: "Falcon 9", company: "SpaceX", location: "Cape Canaveral", date: "2015-12-22", rocket: "Falcon 9", price: 9750000, successful: true,range:75000,spent_time:"0 hrs"},
+    { mission: "Voyager", company: "NASA", location: "Cape Canaveral", date: "1977-09-05", rocket: "Titan-Centaur ", price: 86580000, successful: true,range:65000,spent_time:"50 hrs" },
+    { mission: "Apollo 13", company: "NASA", location: "Kennedy Space Center", date: "1970-04-11", rocket: "Saturn V", price: 3750000, successful: false,range:64000,spent_time:"40 hrs" },
+    { mission: "Falcon 9", company: "SpaceX", location: "Cape Canaveral", date: "2015-12-22", rocket: "Falcon 9", price: 9750000, successful: true,range:75000,spent_time:"0 hrs"},
+    { mission: "Voyager", company: "NASA", location: "Cape Canaveral", date: "1977-09-05", rocket: "Titan-Centaur ", price: 86580000, successful: true,range:65000,spent_time:"50 hrs" },
+    { mission: "Apollo 13", company: "NASA", location: "Kennedy Space Center", date: "1970-04-11", rocket: "Saturn V", price: 3750000, successful: false,range:64000,spent_time:"40 hrs" },
+    { mission: "Falcon 9", company: "SpaceX", location: "Cape Canaveral", date: "2015-12-22", rocket: "Falcon 9", price: 9750000, successful: true,range:75000,spent_time:"0 hrs"},
+    { mission: "Voyager", company: "NASA", location: "Cape Canaveral", date: "1977-09-05", rocket: "Titan-Centaur ", price: 86580000, successful: true,range:65000,spent_time:"50 hrs" },
+    { mission: "Apollo 13", company: "NASA", location: "Kennedy Space Center", date: "1970-04-11", rocket: "Saturn V", price: 3750000, successful: false,range:64000,spent_time:"40 hrs" },
     { mission: "Falcon 9", company: "SpaceX", location: "Cape Canaveral", date: "2015-12-22", rocket: "Falcon 9", price: 9750000, successful: true,range:75000,spent_time:"0 hrs"}
-  ];
+  ]
 
   // Column Definitions: Defines & controls grid columns.
   colDefs: ColDef[] = [
